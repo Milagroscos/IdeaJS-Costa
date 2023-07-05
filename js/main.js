7a1bbb8a368a60ffe2bad0d4d0188a5abfbf5e64
Milagros Costa <milicos10@gmail.com>
2023-07-05 20:30:40 -0300

    agregado de fetch y librerias

--- js/main.js
+++ js/main.js
@@ -1,159 +1,4 @@
-const openMenu = document.querySelector("#open-menu");
-const closeMenu = document.querySelector("#close-menu");
-const aside = document.querySelector("aside");
-
-openMenu.addEventListener("click", () => {
-    aside.classList.add("aside-visible");
-})
-
-closeMenu.addEventListener("click", () => {
-    aside.classList.remove("aside-visible");
-})
-
-let productos= [
-    {
-        id:"campera-grecia",
-        titulo:"Campera Grecia",
-        imagen: "./imagenes/campera-grecia.jpg",
-        categoria: {
-            nombre:"ninia",
-            id:"ninia"
-        },
-        precio: 1200
-    },
-    {
-        id:"deportivo-indiana",
-        titulo:"Deportivo Indiana",
-        imagen: "./imagenes/deportivo-indiana.jpg",
-        categoria: {
-            nombre:"ninia",
-            id:"ninia"
-        },
-        precio: 1300
-    },
-    {
-        id:"jean-tiziana",
-        titulo:"Jean Tiziana",
-        imagen: "./imagenes/jean-tiziana.jpg",
-        categoria: {
-            nombre:"ninia",
-            id:"ninia"
-        },
-        precio: 700
-    },
-    {
-        id:"conjunto-roma",
-        titulo:"Conjunto Roma",
-        imagen: "./imagenes/conjuntos-roma.jpg",
-        categoria: {
-            nombre:"ninia",
-            id:"ninia"
-        },
-        precio: 900
-    },
-    {
-        id:"conjunto-beltran",
-        titulo:"Conjunto Beltran",
-        imagen: "./imagenes/conjunto-beltran.JPG",
-        categoria: {
-            nombre:"ninio",
-            id:"ninio"
-        },
-        precio: 1000
-    },
-    {
-        id:"conjunto-genaro",
-        titulo:"Conjunto Genaro",
-        imagen: "./imagenes/conjunto-genaro.jpg",
-        categoria: {
-            nombre:"bebe",
-            id:"bebe"
-        },
-        precio: 900
-    },
-    {
-        id:"conjunto-lorenzo",
-        titulo:"Conjunto Lorenzo",
-        imagen: "./imagenes/conjunto-lorenzo.jpg",
-        categoria: {
-            nombre:"bebe",
-            id:"bebe"
-        },
-        precio: 900
-    },
-    {
-        id:"conjunto-vicente",
-        titulo:"Conjunto Vicente",
-        imagen: "./imagenes/conjunto-vicente.jpg",
-        categoria: {
-            nombre:"ninio",
-            id:"ninio"
-        },
-        precio: 900
-    },
-    {
-        id:"maya-tiburon",
-        titulo:"Maya Tiburon",
-        imagen: "./imagenes/maya-tiburon.JPG",
-        categoria: {
-            nombre:"ninio",
-            id:"ninio"
-        },
-        precio: 600
-    },
-    {
-        id:"enterito-nieve",
-        titulo:"Enterito Nieve",
-        imagen: "./imagenes/enterito-nieve.jpg",
-        categoria: {
-            nombre:"bebe",
-            id:"bebe"
-        },
-        precio: 1100
-    },
-    {
-        id:"enterito-osito",
-        titulo:"Enterito Osito",
-        imagen: "./imagenes/enterito-osito.jpg",
-        categoria: {
-            nombre:"bebe",
-            id:"bebe"
-        },
-        precio: 1100
-    },
-    {
-        id:"maya-arcoiris",
-        titulo:"Maya Arcoiris",
-        imagen: "./imagenes/maya-arcoiris.jpg",
-        categoria: {
-            nombre:"ninia",
-            id:"ninia"
-        },
-        precio: 600
-    },
-    {
-        id:"tapado-oriana",
-        titulo:"Tapado Oriana",
-        imagen: "./imagenes/tapado-oriana.jpg",
-        categoria: {
-            nombre:"ninia",
-            id:"ninia"
-        },
-        precio: 1500
-    },
-    {
-        id:"vestido-juana",
-        titulo:"Vestido Juana",
-        imagen: "./imagenes.vestido-juana(1).jpg",
-        categoria: {
-            nombre:"ninia",
-            id:"ninia"
-        },
-        precio: 800
-    }
-
-];
-
+let productos = [];
 
 fetch("./js/productos.json")
     .then(response => response.json())
@@ -198,6 +43,7 @@ function cargarProductos(productosElegidos) {
     actualizarBotonesAgregar();
 }
 
+cargarProductos(productos);
 
 botonesCategorias.forEach(boton => {
     boton.addEventListener("click", (e) => {
@@ -226,7 +72,7 @@ function actualizarBotonesAgregar() {
     });
 }
 
-let productosEnCarrito;
+const productosEnCarrito= [];
 
 let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
 
@@ -247,7 +93,7 @@ function agregarAlCarrito(e) {
         position: "right", // `left`, `center` or `right`
         stopOnFocus: true, // Prevents dismissing of toast on hover
         style: {
-          background: "linear-gradient(to right, #4b33a8, #785ce9)",
+          background: "linear-gradient(to right, rgb(99, 99, 33), rgb(211, 211, 175))",
           borderRadius: "2rem",
           textTransform: "uppercase",
           fontSize: ".75rem"
